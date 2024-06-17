b0a3afad005fe3f75344219c57cb286d563f133e
JohanVillard <styx.locke@gmail.com>
2024-06-17 10:55:32 +0000

    Add long int variable in task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
+	long int num = 0;
+
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long int: %lu byte(s)\n", sizeof(num));
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
 }
