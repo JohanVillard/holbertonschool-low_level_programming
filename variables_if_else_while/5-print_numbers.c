ec764888b142ee65cdf9668978a61148738f11e9
JohanVillard <styx.locke@gmail.com>
2024-06-17 16:23:25 +0000

    Add 5-print_numbers.c file and update README

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * use sizeof function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
