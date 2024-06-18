cf9b4db6d35aae08b8c1eee253d9e3632fd31048
JohanVillard <styx.locke@gmail.com>
2024-06-18 20:17:00 +0000

    Update betty rules for 100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int ascii_zero = 48;
-	int ascii_one = 49;
-	int ascii_nine = 57;
+	int ascii0 = 48;
+	int ascii1 = 49;
+	int ascii9 = 57;
 	int first_num;
 	int second_num;
 	int count = 0;
 
-	for (first_num = ascii_zero; first_num < ascii_nine + 1; first_num++)
+	for (first_num = ascii0; first_num < ascii9 + 1; first_num++)
 	{
 
-		for (second_num = ascii_one + count; second_num < ascii_nine + 1; second_num++)
+		for (second_num = ascii1 + count; second_num < ascii9 + 1; second_num++)
 		{
 			putchar(first_num);
 			putchar(second_num);
