1841fa4fa29089536c414a53e8e320ce87355170
JohanVillard <styx.locke@gmail.com>
2024-06-18 17:00:49 +0000

    Update again betty requirements in task 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
 
+/*
+ * main - Entry point
+ *
+ * Description: Display a sentence in STDERR
+ *
+ * Return: 1
+ */
 int main(void)
 {
 	char sentence[] = "and that piece of art is useful\"\
