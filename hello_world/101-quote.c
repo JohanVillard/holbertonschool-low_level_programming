d8326d7e7eff02019f233b3a8ce56529b9a36937
JohanVillard <styx.locke@gmail.com>
2024-06-18 16:57:09 +0000

    Update betty requirements in task 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char sentence[] = "and that piece of art is useful\"\
+- Dora Korpar, 2015-10-19\n";
 
 	write(2, sentence, sizeof(sentence));
 
