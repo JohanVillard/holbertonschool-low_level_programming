ab79889dd4273bb8d8bbb678c74d014741a827aa
JohanVillard <styx.locke@gmail.com>
2024-06-18 13:04:03 +0000

    Add 9-times_tables.c, 9-main.c and update README

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * times_table - print times table
+ *
+ * Description: Display ten lines column * line
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i;
+	int n;
+	int result;
+	int first_num;
+	int second_num;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		for (n = 0; n < 10; n++)
+		{
+			result = i * n;
+			first_num = (result / 10) % 10;
+			second_num = result % 10;
+
+			if (first_num == 0)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + first_num);
+			}
+
+			_putchar('0' + second_num);
+
+			if (n == 9)
+			{
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	_putchar('\n');
+	}
+}
+
