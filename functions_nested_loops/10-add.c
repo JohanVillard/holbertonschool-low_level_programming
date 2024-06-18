786db8c97b6c54e6e6551c673b3988e1151105aa
JohanVillard <styx.locke@gmail.com>
2024-06-18 13:33:01 +0000

    Add 10-add.c, 10-main.c files and update README

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - print sum of 2 numbers
+ *
+ * Description: Add 2 int and display the result
+ *
+ * Return: result of the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
