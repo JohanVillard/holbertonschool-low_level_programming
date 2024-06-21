4f7ef613d405ad2da76a39bda9a4aa2c1a775fe4
JohanVillard <styx.locke@gmail.com>
2024-06-21 18:05:22 +0000

    Display the big number

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,106 @@
 #include "main.h"
 #include <stdio.h>
 
+unsigned long int count_num(unsigned long int num);
+
 /**
  * main - entry point
  *
  * Print 50 first fibonacci numbers
  * Return: 0
  */
+
 int main(void)
 {
 	unsigned long int fib0 = 0;
 	unsigned long int fib1 = 1;
-	unsigned long int fib2;
-	unsigned long int cut_fib1;
-       	unsigned long int cut_fib2;
-	unsigned long int base = 1000;
+	unsigned long int fib2 = 0;
+	unsigned long int count = 0;
+	unsigned long int fib0_start, fib0_end, fib1_start, fib1_end, fib2_start, fib2_end, carry;
 	int i;
 	int limit = 98;
 
 	for (i = 0; i < limit; i++)
-	{
-		fib2 = fib0 + fib1;
-		fib0 = fib1;
-		fib1 = fib2;
-
-		if (i != limit - 1)
+	{	
+		if (count < 20)
 		{
-				cut_fib1 = fib2 / 100000;
-				cut_fib2 = fib2 % 100000;
-				printf("%lu%lu, ", cut_fib1, cut_fib2);
+			fib2 = fib0 + fib1;
+			fib0 = fib1;
+			fib1 = fib2;
 
+			printf("%lu, ", fib2);
+		}
+		else if (fib2_start > 0)
+		{
+			fib0_start = fib1_start;
+			fib1_start = fib2_start;
+			fib0_end = fib1_end;
+                        fib1_end = fib2_end;
+			
+			fib2_end = fib0_end + fib1_end;
+
+                        if (count_num(fib2_end) > count_num(fib1_end))
+                        {
+                                carry = fib2_end / 10000;
+                                fib2_end %= 10000;
+                                fib2_start = fib0_start + fib1_start + carry;
+                        }
+                        else
+                        {
+                                fib2_start = fib0_start + fib1_start;
+                        }
+
+			if (i != limit - 1)
+		       {
+			       printf("%lu%lu, ", fib2_start, fib2_end);
+		       }
+		       else
+		       {
+			       printf("%lu%lu", fib2_start, fib2_end);
+		       }
 		}
 		else
-		{
-			cut_fib1 = fib2 / base;
-                        cut_fib2 = fib2 % base;
-                        printf("%lu%lu", cut_fib1, cut_fib2);
-		}
-	}
+		{	
+			fib0_start = fib0 / 10000;
+			fib0_end = fib0 % 10000;
 
+			fib1_start = fib1 / 10000;
+                        fib1_end = fib1 % 10000;
+
+			fib2_end = fib0_end + fib1_end;
+
+			if (count_num(fib2_end) > count_num(fib1_end))
+			{
+				carry = fib2_end / 10000;
+				fib2_end %= 10000;
+				fib2_start = fib0_start + fib1_start + carry;
+			}
+			else
+			{
+				fib2_start = fib0_start + fib1_start;
+			}
+                        
+			printf("%lu%lu, ", fib2_start, fib2_end);	
+		}	
+		count = count_num(fib2); 
+	}	
 	putchar('\n');
-
 	return (0);
+}
+
+unsigned long int count_num(unsigned long int num)
+{
+	unsigned long int count = 0;
+	
+	if (num == 0)
+       	{
+        	return 1;
+    	}
+
+	while (num != 0)
+	{
+		num /= 10;
+		count++;			   
+	}
+	return (count);
 }
