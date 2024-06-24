d7846f4b582fbbdbc888128fab483f161f576eaa
JohanVillard <styx.locke@gmail.com>
2024-06-24 12:01:20 +0000

    Add 9-fizz_buzz.c file and update README

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fizz-buzz
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
