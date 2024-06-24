fe6a6218acb7bd46723529ee70d972328859755c
JohanVillard <styx.locke@gmail.com>
2024-06-24 08:14:44 +0000

    Add 0-isupper.c and README.md files

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper : check uppercase
+ * @c: char to check
+ *
+ * Return: 1 for uppercase, 0 for lowercase
+ */
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+
+	return (0);
+}
