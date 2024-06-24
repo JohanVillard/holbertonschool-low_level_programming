f632baf928d5cb3a5be9a917eb20b3ee2a9f9480
JohanVillard <styx.locke@gmail.com>
2024-06-24 08:50:13 +0000

    Add 4-print_most_numbers.c file and update README

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9 without 2 an 4
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if ((i == 50) || (i == 52))
+		{
+
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@ Check if case is upper
 Check if digit
 Multiply 2 numbers
 Print 0 to 9 numbers
+Print 0 to 9 numbers 2 and 4 excluded
