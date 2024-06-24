d8e405400da617ef21a51faf98e7e3ed70b94e28
JohanVillard <styx.locke@gmail.com>
2024-06-24 15:51:46 +0000

    Add 4-print_rev.c file and update README

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverse string
+ * @s: array of char
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0, j;
+	char *array = s, letter;
+
+	while (array[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		letter = array[j];
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ Update value with pointer as a paramater of a function
 Swap value
 Return the length of string
 Print a string
+Print a reverse string
