b45824e2e52b52e413580a0d0c4dc8c6890e89ab
JohanVillard <styx.locke@gmail.com>
2024-06-25 20:06:31 +0000

    Fix dump core xin 3-strcmp.c file

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result = 0, i = 1;
+	int result = 0, i = 0;
 
-	result = s1[0] - s2[0];
-
-	while (result == 0)
+	if (*s1 == *s2)
 	{
-		result = s1[i] - s2[i];
-
-		i++;
+		result = 0;
+	}
+	else
+	{
+		while (result == 0)
+		{
+			result = s1[i] - s2[i];
+			i++;
+		}
 	}
 
 	return (result);
