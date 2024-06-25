4f917e2ccde4e9b670777e6fb3d6472b493a068e
JohanVillard <styx.locke@gmail.com>
2024-06-25 20:15:41 +0000

    Change the first loop into a for in task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result = 0, i = 0;
+	int result = 0, i;
 
-	if (*s1 == *s2)
+	for (i = 0; i < s1[i]; i++)
 	{
-		result = 0;
-	}
-	else
-	{
-		while (result == 0)
+		result = s1[i] - s2[i];
+
+		if (result != 0)
 		{
-			result = s1[i] - s2[i];
-			i++;
+			break;
 		}
 	}
 
