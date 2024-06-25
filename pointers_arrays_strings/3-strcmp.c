22d504dd94af440db2d23ba9ef8ec162f0996084
JohanVillard <styx.locke@gmail.com>
2024-06-25 19:43:01 +0000

    Add loop to check each element of arrays in 3-strcmp.c file

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result = 0;
+	int result = 0, i = O;
 
 	result = s1[0] - s2[0];
+
+	while (result == 0)
+	{
+		result = s1[i] - s2[i];
+		i++;
+	}
 
 	return (result);
 
