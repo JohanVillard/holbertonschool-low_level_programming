1aecf6ef53d05e99c79f05c3353b9579ce120534
JohanVillard <styx.locke@gmail.com>
2024-06-26 08:24:14 +0000

    Exclude n value to limit in task 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,15 +12,15 @@ void reverse_array(int *a, int n)
 {
 	int tmp[1000], i, j = 1;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		tmp[i] = *(a + n - j);
 		j++;
 	}
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		*a = tmp[i];
 		a++;
 	}
-}		
+}
