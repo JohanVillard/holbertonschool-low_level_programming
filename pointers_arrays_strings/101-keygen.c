6a9488a1ac8ce9e82532e99b2e5c6ee1ac487aa0
JohanVillard <styx.locke@gmail.com>
2024-06-27 17:35:27 +0000

    Find a solution but dont work in P1T11

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -12,16 +12,36 @@ int main(void)
 {
 	int r_size;
 	int r_num;
+	int sum;
 	int i;
-	char password[16];
+	char password[100];
+	int max_limit;
+	int check_sum;
 
 	srand(time(NULL));
-	r_size = 15;
+	r_size = 1000;
+	sum = 0;
+	max_limit = 126;
+	check_sum = 2772;
 
 	for (i = 0; i < r_size; i++)
 	{
-		r_num = (rand() % (126 - 32 + 1)) + 32;
-		password[i] = r_num;
+		r_num = (rand() % (max_limit - 44 + 1)) + 48;
+		if ((sum + r_num) <= 2709)
+		{	sum += r_num;
+			password[i] = r_num;
+		}
+		else 
+		{
+			r_num = check_sum - sum;
+			password[i]= r_num;
+			password[i + 1] = '\0';
+			sum += r_num;
+		}
+		if (sum == check_sum)
+		{
+			break;
+		}
 	}
 
 	printf("%s", password);
