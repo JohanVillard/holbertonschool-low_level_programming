eb1363c98ffd3cb7d90dc9eb71db3cf55e2bfe05
JohanVillard <styx.locke@gmail.com>
2024-06-28 18:30:46 +0000

    Create a tmp array to reverse in r array in t11

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -14,7 +14,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int n1_len = 0;
 	int n2_len = 0;
-	int i, j, k, num_r, sum_r, carry = 0;
+	int tmp_len = 0;
+	int i, j, k, add_r = 0, sum_r = 0, carry = 0;
+	char tmp[1000] = {0};
 
 	while (n1[n1_len])
 	{
@@ -26,18 +28,42 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		n2_len++;
 	}
 
+	if (size_r < n1_len || size_r < n2_len)
+	{
+		return (0);
+	}
+
 	n1_len--;
 	n2_len--;
 
-	for (i = n1_len, j = n2_len, k = size_r; i > 0; i--, j--, k--)
+	for (i = n1_len, j = n2_len, k = 0; i >= 0 && j >= 0; i--, j--, k++)
 	{
-		num_r = (n1[i] - '0') + (n2[i] - '0') + carry;
-		sum_r = num_r % 10;
-		carry = num_r / 10;
-		r[k] = sum_r;
+		add_r = (n1[i] - '0') + (n2[j] - '0') + carry;
+		sum_r = (add_r % 10);
+		carry = add_r / 10;
+		tmp[k] = sum_r + '0';
 	}
 	
-	printf("RESULT: %s\n", r);
+	carry += '0';
+
+	if (carry != 0)
+	{
+		tmp[k] = carry;
+	}
+
+	tmp[k + 1] = '\0';
+
+	while (tmp[tmp_len])
+        {
+                tmp_len++;
+        }
+
+	tmp_len--;
+	
+	for (i = 0; i < tmp_len; i++, tmp_len--)
+	{
+		r[i] = tmp[tmp_len];
+	}
 
 	return (r);
 }
