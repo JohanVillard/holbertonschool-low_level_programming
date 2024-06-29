fce0b2bbc58d6294f0aa97a10b567c40fc59b2fc
JohanVillard <styx.locke@gmail.com>
2024-06-29 09:55:43 +0000

    Cut string at 10 char in each line in P2T12

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -10,12 +10,24 @@
 void print_buffer(char *b, int size)
 {
 	int address = 0, address_jump = 10, total_lines, line = 0;
+	int i_tmp, i_b = 0;
+	char tmp[10] = {0};
 
 	total_lines = size / 10;
 
-	while (line < total_lines)
+	while (line <= total_lines)
 	{
-		printf("%08x: %s\n", address, b);
+		for (i_tmp = 0; i_tmp < 10; i_tmp++, i_b++)
+		{
+			tmp[i_tmp] = b[i_b];
+		}
+		
+		printf("%08x: %s", address, tmp);
+				
+		if (line != total_lines)
+		{
+			putchar('\n');
+		}
 		
 		address += address_jump;
 		line++;
