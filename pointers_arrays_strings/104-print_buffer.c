f78329ab0f20d1c9f1c57a1a59202f174364c005
JohanVillard <styx.locke@gmail.com>
2024-06-29 18:27:32 +0000

    Correct syntax in P2 T12x

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -18,18 +18,18 @@ void print_buffer(char *b, int size)
 
 	if (size != 0)
 	{
-	while (line <= total_lines)
-	{
-		printf("%08x: ", address);
-
-		for (i = actual_i; i < char_limit; i++)
+		while (line <= total_lines)
 		{
-			if (line != total_lines)
+			printf("%08x: ", address);
+
+			for (i = actual_i; i < char_limit; i++)
 			{
-				printf("%02x", b[i]);
-			}
-			else
-			{
+				if (line != total_lines)
+				{
+					printf("%02x", b[i]);
+				}
+				else
+				{
 				if (b[i] == '\0' && b[i - 1] <= '\0')
 				{
 					printf("  ");	
@@ -42,56 +42,58 @@ void print_buffer(char *b, int size)
 				{
 					printf("%02x", b[i]);
 				}
-			}
-			if (i % 2 != 0)
-			{
-				printf(" ");
-			}
-		}
-		if (line == total_lines)
-		{
-			for (; i < char_limit + (10 - last_line_size); i++)
-			{
-				printf("  ");
+				}
 				if (i % 2 != 0)
 				{
 					printf(" ");
 				}
 			}
-		}
-		for (i = actual_i; i < char_limit; i++)
-		{
-			if ((b[i] == '\0' || b[i] == '\1' || b[i] == '\2'|| b[i] == '\3'
+			if (line == total_lines)
+			{
+				for (; i < char_limit + (10 - last_line_size); i++)
+				{
+					printf("  ");
+					if (i % 2 != 0)
+					{
+						printf(" ");
+					}
+				}
+			}
+			for (i = actual_i; i < char_limit; i++)
+			{
+				if ((b[i] == '\0' || b[i] == '\1' || b[i] == '\2'|| b[i] == '\3'
 				|| b[i] == '\4'|| b[i] == '\5'
 				|| b[i] == '\6'|| b[i] == '\7'
 				|| b[i] == '\n'|| b[i] == '\t'))
+				{
+					printf(".");
+				}
+				else
+				{
+					printf("%c", b[i]);
+				}
+			}
+			putchar('\n');
+		
+			address += address_jump;
+			line++;
+			actual_i = i;
+			if (line != total_lines)
 			{
-				printf(".");
+				char_limit += address_jump;
 			}
 			else
 			{
-				printf("%c", b[i]);
+				char_limit += last_line_size;
 			}
-		}
-		putchar('\n');
-		
-		address += address_jump;
-		line++;
-		actual_i = i;
-		if (line != total_lines)
-		{
-			char_limit += address_jump;
-		}
-		else
-		{
-			char_limit += last_line_size;
-		}
 
-		if (last_line_size == 0 && line == total_lines)
-		{
-			break;
-		}
+			if (last_line_size == 0 && line == total_lines)
+			{
+				break;
+			}
 	}
+	else
+	{
 	printf(" ");
 	}
 }
