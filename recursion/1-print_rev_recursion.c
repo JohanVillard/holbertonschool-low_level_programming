a920e67440319c0af96f7df4acedfe30ab7a5713
JohanVillard <styx.locke@gmail.com>
2024-07-02 07:38:24 +0000

    Apply betty style on P1TO

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+* _print_rev_recursionbe
+*
+*
+*/
+void _print_rev_recursion(char *s)
+{
+
+}
--- recursion/README.md
+++ recursion/README.md
@@ -1,3 +1,4 @@
 Exercise on recursion:
 
 P1T0 : Print a string with recursion
+P1T1 : Print a string in reverse
