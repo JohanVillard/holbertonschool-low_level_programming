9351236c72418ce1d2af8164a0c330b1d8e53696
JohanVillard <styx.locke@gmail.com>
2024-07-02 09:01:57 +0000

    Add P1T3 file and update README

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+* factorial - return factorial of a number
+* @n: int to compute
+*
+* Return: factorial
+*/
+int factorial(int n)
+{
+if (n == 0) 
+{
+return 1;
+}
+else if (n < 0)
+{
+    return (-1);
+}
+return (n * factorial(n - 1));
+}
