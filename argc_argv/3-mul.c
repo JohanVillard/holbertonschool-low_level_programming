51196ff78dc51bdd137d960f52365a38d90441df
JohanVillard <styx.locke@gmail.com>
2024-07-02 15:52:22 +0000

    Add P2T3 file and update README

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+* main - multiply two numbers
+* @argc: count arguments
+* @argv: store arguments in string
+*
+* Return: (0)
+*/
+int main(int argc, char *argv[])
+{
+int result;
+if (argc > 2)
+{
+result = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", result);
+}
+else
+{
+printf("error\n");
+}
+return (0);
+}
--- argc_argv/README.md
+++ argc_argv/README.md
@@ -2,4 +2,5 @@ Exercices portant sur argc et argc
 
 P2TO : Print argv[0]
 P2T1 : Print argc
-P2T1 : Print all arguments that program receives
+P2T2 : Print all arguments that program receives3-mul.c
+P2T3 : Multiply 2 numbers
