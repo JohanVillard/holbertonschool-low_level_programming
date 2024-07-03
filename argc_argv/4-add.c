91a802bea75aabb9844f6576f0d2ec5b45693a3c
JohanVillard <styx.locke@gmail.com>
2024-07-03 14:00:37 +0000

    Remove seond variable for num and initialize sum to 0 in P2T4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,7 +12,7 @@
 */
 int main(int argc, char *argv[])
 {
-int sum, num, i;
+int sum = 0, i;
 
 if (argc > 1)
 {
@@ -20,8 +20,7 @@ for (i = 1; i < argc; i++)
 {
 if ((*argv[i] >= 48) && (*argv[i] <= 57))
 {
-num = atoi(argv[i]);
-sum += num;
+sum += atoi(argv[i]);
 }
 else
 {
