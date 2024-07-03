a21560ffc59ca6e3dec2b4667aa5f28f357ad155
JohanVillard <styx.locke@gmail.com>
2024-07-03 13:03:35 +0000

    Add and correct conditons in P1T4

--- recursion/4-main.c
+++ recursion/4-main.c
@@ -16,7 +16,7 @@ int main(void)
     printf("%d\n", r);
     r = _pow_recursion(2, 16);
     printf("%d\n", r);
-    r = _pow_recursion(5, 2);
+    r = _pow_recursion(0, 0);
     printf("%d\n", r);
     r = _pow_recursion(5, -2);
     printf("%d\n", r);
--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -13,9 +13,17 @@ if (y == 1)
 {
 return (x);
 }
-else if (y <= 0)
+else if (y < 0)
 {
-return (1);
+return (-1);
+}
+else if (y == 0 && x > 0)
+{
+    return (1);
+}
+else if (y == 0 && x == 0)
+{
+    return (0);
 }
 return (x * _pow_recursion(x, y - 1));
 }
