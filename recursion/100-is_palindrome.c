a179a867662588d0d6c128aa1afcd256436a341f
JohanVillard <styx.locke@gmail.com>
2024-07-04 17:54:58 +0000

    Apply betty style to P1T7

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -8,15 +8,15 @@
  */
 int is_palindrome(char *s)
 {
-   int len;
+	int len;
 
-    if (*s == '\0')
-    {
-      return (1);
-    }
+	if (*s == '\0')
+	{
+		return (1);
+	}
 
-    len = _strlen_recursion(s) - 1;
-    return (check(s, s + len));
+	len = _strlen_recursion(s) - 1;
+	return (check(s, s + len));
 }
 
 /**
@@ -28,16 +28,17 @@ int is_palindrome(char *s)
  */
 int check(char *start, char *end)
 {
-    if (start >= end)
-  {
-    return (1);
-  }
+	if (start >= end)
+	{
+	return (1);
+	}
 
-  if (*start != *end)
-  {
-    return (0);
-  } return check (start + 1, end - 1);
-}
+	if (*start != *end)
+	{
+	return (0);
+	}
+	return (check(start + 1, end - 1));
+	}
 
 /**
 * _strlen_recursion - return length of a string
@@ -47,9 +48,9 @@ int check(char *start, char *end)
 */
 int _strlen_recursion(char *s)
 {
-    if (*s)
-    {
-      return (1 + _strlen_recursion(s + 1));
-    }
-      return (0);
-    }
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+		return (0);
+	}
