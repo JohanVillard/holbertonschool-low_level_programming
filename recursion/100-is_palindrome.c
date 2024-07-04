a8f4e0fc719a0c2d6eef1cfd80adc52637fc97b0
Johan <styx.locke@gmail.com>
2024-07-04 18:56:02 +0200

    Add P1T7 file

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * is_palindrome - it is ?
+ *
+ * Return: 1 if true if 0 false
+ */
+int is_palindrome(char *s)
+{
+   int len;
+   
+    if (s == '\0')
+    {
+        return 1;
+    }
+
+  len = _strlen_recursion(s) - 1;
+  
+    return (check(s, s + len));
+}
+
+/**
+ * check - check if first and last char are equal
+ *
+ * Return: 1 if true if 0 false
+ */
+int check(char *start, char *end)
+{
+    if (start >= end)
+  {
+    return 1;
+  }
+  
+  if (*start != *end)
+  {
+    return 0;
+  }
+
+    return check(start + 1, end - 1);
+}
+
+
+int _strlen_recursion(char *s)
+{
+    if (*s)
+    {
+        return (1 + _strlen_recursion(s + 1));
+    }
+        return (0);
+    }
