874c1e879db45aa746da74bc067c092e4378d6f4
JohanVillard <styx.locke@gmail.com>
2024-07-08 11:54:38 +0000

    Adds is NULL conditons in P0T2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,6 +14,15 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0;
 	int len1 = 0, len2 = 0, len3 = 0;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	
 	while (s1[len1])
 	{	len1++;	}
 	while (s2[len2])
