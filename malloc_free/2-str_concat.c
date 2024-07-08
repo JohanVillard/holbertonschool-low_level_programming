96909ffd8d94f7e0d700e4414d3665dca593eeed
JohanVillard <styx.locke@gmail.com>
2024-07-08 11:20:11 +0000

    Correct condition  malloc allocation after his call in P0T2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,46 +15,40 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0, len3 = 0;
 
 	while (s1[len1])
-	{
-		len1++;
-	}
-
+	{	len1++;	}
 	while (s2[len2])
-	{
-		len2++;
-	}
-
+	{	len2++;	}
 	len3 = len1 + len2 + 1;
-
 	/* Alloue la mémoire à tmp (taille de str par la taille d'une char) */
 	tmp = malloc(sizeof(char) * len3);
-
-	while (i < len1)
-	{
-		if (s1 == NULL)
-		{
-			tmp = "";
-		}
-		tmp[i] = s1[i];
-		i++;
-	}
-
-	while (j < len2)
-	{
-		if (s2 == NULL)
-		{
-			tmp = "";
-		}
-		tmp[i + j] = s2[j];
-		j++;
-	}
-
-	tmp[i + j] = '\0';
-
+	/* Alloue la mémoire à tmp (taille de str par la taille d'une char) */
 	if (tmp == NULL)
+	{	return (NULL);	}
+	if (s1 == NULL)
+	{	tmp = "";	}
+	else
 	{
-		return (NULL);
+		while (i < len1)
+		{
+			if (s1 == NULL)
+			{
+				tmp = "";
+			}
+			tmp[i] = s1[i];
+			i++;
+		}
 	}
-
+	if (s2 == NULL)
+	{	tmp = "";	}
+	else
+	{
+		while (j < len2)
+		{
+			tmp[i + j] = s2[j];
+			j++;
+		}
+	}
+	tmp[i + j] = '\0';
 	return (tmp);
 }
+
