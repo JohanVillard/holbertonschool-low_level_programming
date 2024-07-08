4fde76ad74fcf578caf55194ac9a0dac234bd30f
JohanVillard <styx.locke@gmail.com>
2024-07-08 13:58:33 +0000

    Add P0T4 file and update README

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * free_grid - free memry from 2D array
+ * @grid: integer 2D grid
+ * @height: integer line
+ * Return: (s)
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
+
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -4,3 +4,4 @@ P0T0 : Create an array with malloc
 P0T1 : Duplicate a string
 P0T2 : Concatene 2 strings
 P0T3 : Create a double array with malloc
+P0T4 : Free momory from a 2D array
