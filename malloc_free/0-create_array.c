3693bcc75ca6b1da2878cbd48b00f059aa31157c
JohanVillard <styx.locke@gmail.com>
2024-07-08 08:38:46 +0000

    Add P0T0, README, main.h files

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,31 +8,31 @@
  * @c: element to insert in array
  * Return: ()
  */
- char *create_array(unsigned int size, char c)
- {
-    char *s;
-    unsigned int i;
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
 
-    /* Alloue la mémoire à s (size par la taille d'une char) */
-    s = malloc(size * sizeof(char));
+	/* Alloue la mémoire à s (size par la taille d'une char) */
+	s = malloc(size * sizeof(char));
 
-    if (size == 0)
-    {
-        return NULL;
-    }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-    i = 0;
-    while (i < size)
-    {
-        s[i] = c;
-        i++;
-    }
-    s[i] = '\0';
-    
-    if (s == NULL)
-    {
-        return (NULL);
-    }
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
 
-    return (s);
- }
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	return (s);
+}
