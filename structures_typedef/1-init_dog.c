c11c3d799213498a38bed4793a60f53f3c75440a
JohanVillard <styx.locke@gmail.com>
2024-07-09 12:56:59 +0000

    Apply betty style in P2T1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -17,7 +17,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	{
 		exit(1);
 	}
-	
+
 	/* Initialise tous les paramètres de la struc créée dans ailleurs */
 	d->name = name;
 	d->owner = owner;
