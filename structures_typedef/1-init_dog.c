3a748ff2dcaeaca98fd25b2c11edd0fdd369a20b
JohanVillard <styx.locke@gmail.com>
2024-07-09 12:47:30 +0000

    Delete all conditons except struct check in P2T1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,35 +14,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	/* Memory check */
 	if (d == NULL)
 	{
-		free(d);
 		exit(1);
 	}
 
 	/* Initialise tous les paramètres de la struc créée dans ailleurs */
-	/* Check chaque pointer*/
-	if (*name == '\0')
-	{
-		free(d);
-		exit(1);
-	}
-
 	d->name = name;
-
-	if (*owner== '\0')
-	{
-		free(name);
-		free(d);
-		exit(1);
-	}
 	d->owner = owner;
-
-	if (age < 0)
-	{
-		free(owner);
-		free(name);
-		free(d);
-		exit(1);
-	}
 	d->age = age;
 }
 
