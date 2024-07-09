74328c29d350d947818954b33447c521fe52b505
JohanVillard <styx.locke@gmail.com>
2024-07-09 05:53:42 +0000

    Add P1T2 file and update README

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,44 +11,30 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	/* Déclare un pointeur */
 	char *ptr;
-	/* Initialise les longueurs de tableaux et compteurs*/
-	unsigned int len1 = 0, len2 = 0, n_s2 = 0, i, j;
+	/* Déclare un compteur */
+	unsigned int i;
 
-	/* Taille de s1 et s2 sans l'octet de fin*/
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	/* Compare s2 à n */
-	if (n >= len2)
+	/* Stop la fonction si la taille n'est pas supérieure à zéro */
+	if (size == 0)
 	{
-		/* Longueur tout s2 */
-		n_s2 = len2;
-	}
-	else
-	{
-		/* Longueur n bytes de s2 */
-		n_s2 = n;
-	}
-	/* Initialise le pointeur à l'adresse donnée par malloc */
-	/* La string concaténée + l'octet de fin*/
-	ptr = malloc(len1 + n_s2 + 1);
-	/* Allocation réussie ? */
-	if (ptr == NULL)
-	{
-		/* Renvoie NULL si non */
 		return (NULL);
 	}
-	/* Incrémente s1 dans la nouvelle string */
-	for (i = 0; i < len1; i++)
+
+	/* Alloue la mémoire ode nmemb of nsize */
+	ptr = malloc(nmemb * size);
+
+	/* Allocation réussie ?*/
+	if (ptr == NULL)
 	{
-		ptr[i] = s1[i];
+		return (NULL);
 	}
-	/* Incrémente s2 à n bytes dans la nouvelle string */
-	for (j = 0; j < n_s2; j++)
+
+	/* Initialise tout les membres à zero */
+	for (i = 0; i < nmemb; i++)
 	{
-		ptr[i + j] = s2[j];
+		ptr[i] = 0;
 	}
-	/* Termine la string avec l'octect de fin */
-	ptr[i + j] =  '\0';
+
 	return (ptr);
 }
 
