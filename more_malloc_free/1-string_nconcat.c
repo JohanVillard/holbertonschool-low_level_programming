98106987977d9a04d460963d197ffcd5cfb22237
JohanVillard <styx.locke@gmail.com>
2024-07-09 06:04:14 +0000

    Add NULL replace conditions in P1T1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	/* Initialise les longueurs de tableaux et compteurs*/
 	unsigned int len1 = 0, len2 = 0, n_s2 = 0, i, j;
 
+	/* If s1 or s2  is NULL */
+	if (s1 == NULL)
+	{	s1 = ""; }
+	if (s2 == NULL)
+	{	s2 = ""; }
+
 	/* Taille de s1 et s2 sans l'octet de fin*/
 	while (s1[len1])
 	{	len1++;	}
@@ -28,24 +34,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{	/* Longueur n bytes de s2 */
 		n_s2 = n;
 	}
-	/* Initialise le pointeur à l'adresse donnée par malloc */
 	/* La string concaténée + l'octet de fin*/
 	ptr = malloc(len1 + n_s2 + 1);
 	/* Allocation réussie ? */
 	if (ptr == NULL)
-	{	/* Renvoie NULL si non */
-		return (NULL);
-	}
+	{	return (NULL);	}
 	/* Incrémente s1 dans la nouvelle string */
 	for (i = 0; i < len1; i++)
-	{
-		ptr[i] = s1[i];
-	}
+	{	ptr[i] = s1[i];	}
 	/* Incrémente s2 à n bytes dans la nouvelle string */
 	for (j = 0; j < n_s2; j++)
-	{
-		ptr[i + j] = s2[j];
-	}
+	{	ptr[i + j] = s2[j];	}
 	/* Termine la string avec l'octect de fin */
 	ptr[i + j] =  '\0';
 	return (ptr);
