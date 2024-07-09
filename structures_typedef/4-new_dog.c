67e98e3051e0907cdfd0632a0382e395a0e15705
JohanVillard <styx.locke@gmail.com>
2024-07-09 15:25:52 +0000

    Remove double incrementation of '\0' in P2T4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,42 +11,41 @@
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	/* Déclare une structure */
+	/* Déclare une structure et les longueurs de string */
 	dog_t *d;
-	/* Déclare les longueurs de string */
 	int len1 = 0, len2 = 0, i;
 
-	/* Alloue la mémoire à la struct */
+	/* Alloue la mémoire à la struct and check allocation */
 	d = malloc(sizeof(dog_t));
-	/* Check allocation */
 	if (d == NULL)
 	{	return (NULL);	}
-	/* Longueur de name et owner */
+
+	/* Longueur de name et owner et ajout de l'octet de fin */
 	while (name[len1])
 	{	len1++;	}
+	len1++;
 	while (owner[len2])
 	{	len2++;	}
-	/* Ajout du caractère de fin */
-	len1++;
 	len2++;
-	/* Alloue et libère (si echec) la mémoire des strings*/
+
+	/* Alloue et libère (si echec) la mémoire des strings */
 	d->name = malloc(len1 * sizeof(char));
 	if (d->name == NULL)
-	{
-		free(d);
+	{	free(d);
 		return (NULL);	}
 	d->owner = malloc(len2 * sizeof(char));
 	if (d->owner == NULL)
-	{
-		free(name);
+	{	free(name);
 		free(d);
 		return (NULL);	}
-	/* Copy name dans d->name owner dans d->owner */
+
+	/* Copy name dans d->name et owner dans d->owner */
 	for (i = 0; i < len1; i++)
 	{	d->name[i] = name[i];	}
+
 	for (i = 0; i < len2; i++)
 	{	d->owner[i] = owner[i];	}
-	/* Attribue age à age de la struct d */
-	d->age = age;
+
+	d->age = age;	/* Attribue age à age de la struct d */
 	return (d);
 }
