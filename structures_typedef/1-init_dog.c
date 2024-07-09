bac26b51edc95fda0f377e06310af8aded14932b
JohanVillard <styx.locke@gmail.com>
2024-07-09 12:58:59 +0000

    Exclude 0 in paramaters validations and factorize in P2T1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,7 +13,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
 	/* Memory and parameters check */
-	if (d == NULL || name == NULL || owner == NULL || age < 0)
+	if (d == NULL || name == NULL || owner == NULL || age <= 0)
 	{
 		exit(1);
 	}
