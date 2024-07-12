b9c83ffc471feb4b3f70653ae52cf68886a66dde
Johan <styx.locke@gmail.com>
2024-07-12 11:26:27 +0200

    Remove test in P2T5

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	if (argc == 3) /* Cmd + num1 + num2 */
 	{		if (argv[1][0] == '0' || argv[2][0] == '0')	/* Cas zéro */
 		{
-
+			printf("0\n"); /* Le système gère la mem de argv*/
 			return (0);	/* Pas  de fuite de mémoire */
 		}
 		for (i = 1; i < argc; i++) /* Pointe chacun de nums */
