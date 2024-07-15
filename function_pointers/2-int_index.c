472e640f1eeea2f62c1d9273800230e9751aefa7
Johan <styx.locke@gmail.com>
2024-07-15 15:58:58 +0200

    Simplify a lot P0T2 like P0T0

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,28 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size == 0)
 		return (-1);
 
-	if (cmp == &is_98)	/* Adresse égale ? */
+	for (i = 0; i < size; i++)	/* Parcours array */
 	{
-		for (i = 0; i < size; i++)	/* Parcours array */
-			if (is_98(*(array + i)))	/* si la valeur du tableau à l'index i = 98 */
-				return (i);	/* Renvoie l'index i si la valeur est trouvée */
-		return (-1);	/* Renvoie moins si pas de correspondance */
+		if (cmp(*(array + i)))	/* si la valeur du tableau à l'index i = 98 */
+			return (i);	/* Renvoie l'index i si la valeur est trouvée */
 	}
 
-	if (cmp == &is_strictly_positive)	/* Adresse égale ? */
-	{
-		for (i = 0; i < size; i++)	/* Parcours array */
-			if (is_strictly_positive(*(array + i)))	/* si la val du tab à i = 98 */
-				return (i);	/* Renvoie l'index i si la valeur est trouvée */
-		return (-1);	/* Renvoie moins si pas de correspondance */
-	}
-
-	if (cmp == &abs_is_98)	/* Adresse égale ? */
-	{
-		for (i = 0; i < size; i++)	/* Parcours array */
-			if (abs_is_98(*(array + i)))	/* si la valeur du tableau à l'index i = 98 */
-				return (i);	/* Renvoie l'index i si la valeur est trouvée */
-		return (-1);	/* Renvoie moins si pas de correspondance */
-	}
-	return (-1); /* La fonction ne renvoie pas 0 */
+	return (-1); /* Renvoie moins si pas de correspondance */
 }
