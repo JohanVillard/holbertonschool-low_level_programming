3c057ba1b7513c5d8c0a61df59a0550fd8cdaca3
Johan <styx.locke@gmail.com>
2024-07-15 15:45:52 +0200

    Call pointer f in P0T0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -10,10 +10,10 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (f == &print_name_as_is)	/* Si l'ad de f = à l'ad de print_name_as_is */
 	{
-		print_name_as_is(name);
+		f(name);
 	}
 	else if (f == &print_name_uppercase)	/* Si ad de f = ad print_name_uppercas */
 	{
-		print_name_uppercase(name);
+		f(name);
 	}
 }
