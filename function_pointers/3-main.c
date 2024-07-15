cf692d24457f3db97b39ca3ccf7fdf6da2943063
Johan <styx.locke@gmail.com>
2024-07-15 17:04:15 +0200

    Correct div and mod by 0 condition in POT3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,8 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = 0, b = 0;	/* Nombre à calculer */
-	/*int (*pf)(int, int); /* Pointeur de fonction */
+	int a = 0, b = 0, result = 0;	/* Nombre à calculer */
+	/*int (*pf)(int, int);Pointeur de fonction */
 
 	if (argc != 4)	/* Vérifie le nombre d'arguments */
 	{
@@ -27,8 +27,8 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] != '%')
-	&& *argv[3] == 0)	/* Vérifie la div ou le modulo par 0 */
+	if ((*argv[2] == '/' || *argv[2] == '%')	/* Vérif la div ou le mod par 0 */
+	&& *argv[3] == 48)	/* Seule le code ASCII du zéro fonctionne... */
 	{
 		printf("Error\n");
 		exit(100);
@@ -39,6 +39,8 @@ int main(int argc, char *argv[])
 	/* On récupère l'adresse de la function de calcul */
 	/* que la fonction get_func a choisi suivant l'opérateur */
 	/* Puis le résultat est calculé avec les paramètres a et b*/
-	printf("%d\n", get_op_func(argv[2])(a, b));	/* Affiche le résultat stocké */
+	result = get_op_func(argv[2])(a, b);
+
+	printf("%d\n", result);	/* Affiche le résultat stocké */
 	return (0);
 }
