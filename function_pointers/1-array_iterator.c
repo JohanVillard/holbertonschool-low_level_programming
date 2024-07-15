1efb5a76006fd74fe4064b2aa3074e369144ea21
Johan <styx.locke@gmail.com>
2024-07-15 15:54:57 +0200

    Simplify a lot P0T1 like P0T0

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,11 +11,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i = 0; /* Compteur */
 
-	if (action == &print_elem)	/* Même adresse ? */
-		for (i = 0; i < size; i++)	/* Parcours array */
-			print_elem(*(array + i));	/* Affiche la valeur à l'index i */
-
-	else if (action == &print_elem_hex)	/* Même adresse ? */
-		for (i = 0; i < size; i++)	/* Parcours array */
-			print_elem_hex(*(array + i));	/* Affiche la valeur à l'index i */
+	for (i = 0; i < size; i++)	/* Parcours array */
+		action(*(array + i));	/* Affiche la valeur à l'index i */
 }
