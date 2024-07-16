15bc1338a2e40a71ff6924cac9c65fe5aef739cd
Johan <styx.locke@gmail.com>
2024-07-16 16:56:02 +0200

    Test P1T0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -17,8 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(nums, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(nums, int);	/* Renvoie les arguments successivement */
-									/* de nums de type spécifié dans l'appel */
+		sum += va_arg(nums, int);
 
 	va_end(nums);		/* Met fin à l'utilisation de nums */
 
