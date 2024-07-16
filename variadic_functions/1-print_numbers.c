c8354f59beb1d5c5b63d3aa3ee104a1dee570f7c
Johan <styx.locke@gmail.com>
2024-07-16 08:17:14 +0200

    Test in P1T1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,15 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list nums;	/* Déclare un pointeur d'argument */
 
-
-
 	va_start(nums, n);	/* Initialisation */
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nums, int));	/* Passe d'arg en arg */
-		if (i != n - 1)	/* N'affiche pas à la dernière occurence */
-			printf("%s", separator);
+		if (i != n - 1  && separator != NULL)	/* Ne s'affiche pas */
+			printf("%s", separator);	/* à la dernière occurence et si NULL */
 	}
 
 	va_end(nums);	/* On a fini d'utiliser nums */
