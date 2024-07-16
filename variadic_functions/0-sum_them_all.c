0cf368d250d02982a0bd63413f33a8f55f614d44
Johan <styx.locke@gmail.com>
2024-07-16 16:31:03 +0200

    Test P1T0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,9 +12,6 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;		/* Somme */
 	va_list nums;		/* Déclare un pointeur d'argument */
 
-	if (n == 0)
-		return (0);
-
 	va_start(nums, n);	/* Initialise nums pour qu'elle pointe */
 						/* vers le premier argument optionnel */
 						/* 'n' étant le param nommé précédant le premier param variable*/
