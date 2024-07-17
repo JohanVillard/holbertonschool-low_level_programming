0484a13ac849bcb86d754cdde72d04ec6303c2a0
Johan <styx.locke@gmail.com>
2024-07-17 15:42:54 +0200

    P0T3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -14,19 +14,17 @@ void print_all(const char * const format, ...)
 	va_list datas;	/* Déclare un pointeur d'argument */
 	char spe, *t_string;	/* Spécificateur à imprimer, Stock string arg*/
 	const char *prt_format = format;	/* prt_format constant*** */
-	int count_spe = 0;	/* Compteur de spécificateur de format */
 
 	va_start(datas, format);	/* Initialisation */
 	while (*prt_format != '\0')	/* Parcourt format */
 	{
 		spe = *prt_format;	/* Attribue la lettre pointée actuellement vers format */
-		if ((count_spe != 0) && (spe == 's' || spe == 'i'
+		if ((spe == 's' || spe == 'i'
 		|| spe == 'f' || spe == 'c'))	/* Si le caractère suivant */
 			printf(", ");	/* n est pas la dernière occurence*/
 		switch (spe)	/* Pour la valeur de spe */
 		{
 			case 's':	/* Print string */
-				count_spe++;	/* Compte le nombre de spe valide */
 				t_string = va_arg(datas, char *);
 				if (t_string == NULL)	/* Si la string est NULL */
 				{
@@ -36,19 +34,15 @@ void print_all(const char * const format, ...)
 				printf("%s", t_string);
 				break;
 			case 'i':	/* Print integer */
-				count_spe++;	/* Compte le nombre de spe valide */
 				printf("%i", va_arg(datas, int));
 				break;
 			case 'f':	/* Print float */
-				count_spe++;	/* Compte le nombre de spe valide */
 				printf("%f", va_arg(datas, double));
 				break;
 			case 'c':	/* Print char */
-				count_spe++;	/* Compte le nombre de spe valide */
 				printf("%c", va_arg(datas, int));
 				break;
 			default:
-				count_spe++;	/* Compte le nombre de spe valide */
 				break;
 		}
 		prt_format++;	/* déplace le pointeur à l'adresse suivante */
