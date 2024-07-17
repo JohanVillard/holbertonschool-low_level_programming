294439db578e306e0e1fe140a9acf92fd35ce848
Johan <styx.locke@gmail.com>
2024-07-17 14:57:07 +0200

    P1T4

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,20 +1,18 @@
-section .note.GNU-stack
+			global 		main							;DIRECTIVE: 		INSTRUCTION OPERANDS		;Définition de main à cause de gcc à la place de start - pas besoin de ld... pas de global _start	;On est dans le main comme en C.
 
-section .data
-	msg db 'Hello, World!', 0xA			;Message à imprimer + byte de fin(0xA)
-	len equ $ -msg						;Longueur du message(len) =(equ/calcule) adresse actuelle($) - l'adresse du message(msg)
+			section 	.text							;SECTION:			INSTRUCTION OPERANDS		;Déclare la section .text
+main:		mov			rax, 1							;			LABEL: 	INSTRUCTION OPERANDS		;Charge 1 dans rax correspondant à sys_write (appel système pour écrire)	;rax -- r = 64 BITS; a = 
+			mov			rdi, 1							;					INSTRUCTION OPERANDS		;Charge 1 dans rdi. Affichera sur stdout (File descriptor 1)
+			mov			rsi, message					;       			INSTRUCTION OPERANDS		;Charge l'adresse du message dans rsi
+			mov			rdx, len						;					INSTRUCTION OPERANDS		;Charge la longueur du message dans rdx
+			syscall										;					INSTRUCTION					;Fait l'appel système pour afficher le msg
 
-section .text							;Déclare la section .text
-	global _start						;Doit être déclaré pour que l'éditeur de liens(linker) trouve l'entrée du programme
+    		mov 		rax, 60                 		;					INSTRUCTION OPERANDS		;Charge le numéro d'appel du système (60: sys_exit)
+    		xor 		rdi, rdi                 		;					INSTRUCTION OPERANDS		;Met à zéro le code de sortie (0)
+    		syscall                      				;					INSTRUCTION					;Fait l'appel du système pour sortir du programme
 
-_start:									;Dis a l'éditeur de lien que c'est le point d'entrée
-	;Appel du sytème de sys_write
-	mov rdx, len						;Charge la longueur du message dans rdx
-	mov rsi, msg						;Charge l'adresse du message dans rsi
-	mov rax, 1							;Charge 1 dans rax pour sys_write (appel système pour écrire)															
-	syscall								;Fait l'appel système pour afficher le msg
+						section 	.data				;SECTION:			INSTRUCTION OPERANDS
+message:	db 			'Hello, World!', 0xa			;			LABEL: 	INSTRUCTION OPERANDS		;db: Declare Byte; Message à imprimer + saut de ligne
+			len 		equ $ - message					;					INSTRUCTION OPERANDS		;Longueur du message(len) =(equ/calcule) adresse actuelle($) - l'adresse du message(message)
 
- 	;Appel du sytème de sys_exit
-    mov rax, 60                 		;Charge le uméro d'appel du sytème (60: sys_exit)
-    xor rdi, rdi                 		;Met à zéro le code de sortie (0)
-    syscall                      		;Fait l'appel du système pour sortir du programme
+			section 	.note.GNU-stack					;SECTION:			INSTRUCTION OPERANDS
