90a9f9e7ec1368c6a9ce53950803067d57868f9a
Johan <styx.locke@gmail.com>
2024-07-17 09:04:37 +0200

    Test P1T0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;					/* Somme */
 
 	if (n == 0)						/* Sort du code si n = 0 */
-	return (0);
+		return (0);
 
 	va_start(term, n);				/* Init le pointeur */
 	for (i = 0; i < n; i++)
