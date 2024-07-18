22647c0f9859c9d0b3f9cac9d8468296ff0e6e26
Johan <styx.locke@gmail.com>
2024-07-18 18:34:55 +0200

    P1T4

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,8 +13,8 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list datas;									/* Déclare un pointeur d'argument */
-	char spe;										/* Spécificateur de format */
+va_list datas;									/* Déclare un pointeur d'argument */
+char spe;										/* Spécificateur de format */
 	const char *ptr_format = format;
 	int i = 0;									/* Compteur */
 	specifier specifiers[] = {					/* Stocke les choix dans une structure */
@@ -23,30 +23,29 @@ void print_all(const char * const format, ...)
 		{"f", print_float},
 		{"c", print_char},
 		{NULL, NULL}							/* Indique la fin de la structure */
-	};										/* Ptr constant vers format */
+	};											/* Ptr constant vers format */
 
-	va_start(datas, format);				/* Initialisation */
-	while (*ptr_format != '\0')				/* Ptr parcourt format */
+	va_start(datas, format);					/* Initialisation */
+	while (*ptr_format != '\0')					/* Ptr parcourt format */
 	{
-		spe = *ptr_format;					/* spe pointe vers char actuel de format */
+		spe = *ptr_format;						/* spe pointe vers char actuel de format */
 
 		while (specifiers[i].spec != NULL)		/* Tant la val.spec != NULL*/
 		{
-			if (spe == *(specifiers[i].spec))	/* spe correspond à ceux de la struct */
+			if (spe == *(specifiers[i].spec))	/* spe correspond à ceux de la struct*/
 			{
-				(specifiers[i].f(datas));			/* Appel la fonction correspondante */
+				(specifiers[i].f(datas));
+				printf(", ");					/* Appel la fonction correspondante */
 			}
 			i++;
 		}
-		i = 0;										/* Reset du compteur sinon la boucle ne se relance pas */
-		if ((*(ptr_format + 1) != '\0') &&			/* Virgule ou pas... */
-		(spe == 's' || spe == 'i' || spe == 'f' || spe == 'c'))
-			printf(", ");
+		i = 0;									/* Reset du compteur sinon la boucle ne se relance pas */
 
-		ptr_format++;								/* Place le pointeur à l'adresse suivante */
+	ptr_format++;								/* Place le pointeur à l'adresse suivante */
 	}
-	va_end(datas);									/* Fin d'utilisation de datas */
-	printf("\n");
+	va_end(datas);								/* Fin d'utilisation de datas */
+
+	printf("\b\b \n");							/* Efface le carac de fin et la virgule */
 }
 
 /**
@@ -60,7 +59,11 @@ void print_string(va_list datas)
 	char *t_string;
 
 	t_string = va_arg(datas, char *);
-
+	if (t_string == NULL)						/* Si la string est NULL */
+	{
+		printf("(nil)");
+		return;									/* Affiche (nil) */
+	}
 	printf("%s", t_string);						/* Stocke va_arg si il est une string */
 }
 
