ca8e87250ac53f48ef9a79bf450d8aafe1016949
JohanVillard <styx.locke@gmail.com>
2024-07-29 22:38:03 +0200

    Re add add_dnodeint function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,22 +14,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *curr, *before;
 	unsigned int count = 0;	/* Inde bgin at 0 */
 
-	new_node = malloc(sizeof(dlistint_t));	/* Allocate memory */
-	if (new_node == NULL)	/* Malloc check */
-		return (NULL);
-
-	new_node->n = n;	/* Attribute value of n */
+	new_node = add_dnodeint(h, n);
 
 	curr = *h;	/* Take the address of head */
 
 	while (count < idx)
 	{
 		curr = curr->next;	/* Address to take when loop is over */
-		count++;
-		if (curr == NULL)	/* idx does not  */
+		if (curr == NULL)	/* idx does not exist */
 			return (NULL);
+		count++;
 	}
-
 
 	before = curr->prev;	/* Take the address of the list before new_node */
 
