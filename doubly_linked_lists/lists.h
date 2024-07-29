80836a18edf80ebd9717571b7fc5b7bcc27ee7d5
JohanVillard <styx.locke@gmail.com>
2024-07-29 21:41:59 +0200

    Add insert_dnodeint_at_index prototype

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -37,5 +37,7 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 /* P1T6 */
 int sum_dlistint(dlistint_t *head);
+/* P1T7 */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif /* LISTS_H */
