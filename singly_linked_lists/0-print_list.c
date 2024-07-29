8e8516528e7a722ed26850a502d52d9fa76cde2a
JohanVillard <styx.locke@gmail.com>
2024-07-29 09:24:51 +0200

    Add print_list feature

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,9 +4,23 @@
  * print_list - Prints all the elements of a list_t list.
  * @h: Lists to print.
  *
- * Return:  The number of nodes
+ * Return: The number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	
+	size_t length = 0;    /* Linked list length */
+
+	while (h != NULL)	/* While the head don't reach the end */
+	{
+		if (h->str == NULL)	/* If string is empty */
+			printf("[0] (nil)\n");	/* Hard coded printf */
+		else
+		  printf("[%d] %s\n", h->len, h->str);
+
+		length++;    /* Add 1 at each iteration*/
+
+		h = h->next;	/* Move the head to the next node */
+	}
+
+	return (length);
 }
