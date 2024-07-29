d91e1ba9d724376e874822f6c53c066a0edfcbc6
JohanVillard <styx.locke@gmail.com>
2024-07-29 11:10:46 +0200

    Remove strlen() and implement it

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;	/* Declare a new node */
 	list_t *temp;	/* Temporary head */
+	unsigned int str_length = 0;
 
 	new = malloc(sizeof(list_t));	/* Assign memory to new node */
 	if (new == NULL)				/* Malloc check */
@@ -19,7 +20,13 @@ list_t *add_node(list_t **head, const char *str)
 	temp = *head;	/* Store the actual head adress */
 	*head = new;	/* The head point the new node */
 	new->str = strdup(str);	/* Copy string in struct */
-	new->len = strlen(str);	/* Count the length of string */
+
+	while (*str)	/* Count string's length */
+	{
+		str_length++;
+		str++;
+	}
+	new->len = str_length;	/* Count the length of string */
 	new->next = temp;	/* The new becomes the first node */
 
 	return (new);
