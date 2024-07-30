b949f0411cc797ae4faf310c969695cbbcf6c125
JohanVillard <styx.locke@gmail.com>
2024-07-30 18:28:38 +0200

    Ckeck if n is negative

--- bit_manipulation/2-main.c
+++ bit_manipulation/2-main.c
@@ -10,6 +10,10 @@ int main(void)
 {
 	int n;
 
+	n = get_bit(-1, 0);
+	printf("%d\n", n);
+	n = get_bit(1024, 1025);
+	printf("%d\n", n);
 	n = get_bit(1024, 10);
 	printf("%d\n", n);
 	n = get_bit(98, 1);
