075ce4c9e5062aec8f0e8fd41500a383553321a2
JohanVillard <styx.locke@gmail.com>
2024-07-30 22:54:18 +0200

    Add free and close to error handling

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,31 +13,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd = 0;
 	char *buffer;	/* Create a buffer */
 	/* ssize_t represents the size of an allocated block of memory, but signed  */
-	ssize_t writeBytes = 0, readBytes = 0;
+	ssize_t write_bytes = 0, read_bytes = 0;
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);	/* Open file */
 	if (fd == -1)	/* Open check */
 		return (0);
-
 	buffer = malloc(letters);	/* Allocate letters bytes to the buffer */
 	if (buffer == NULL)	/* Malloc check */
+	{
+		close(fd);	/* Don't forget to close file !!! */
 		return (0);
-
-	/* Put data in the buffer to letters bytes */
-	readBytes = read(fd, buffer, letters);
-	if (readBytes == -1)	/* Open check */
+	}
+	/* Store data from the file fd in the buffer to letters bytes */
+	read_bytes = read(fd, buffer, letters);
+	if (read_bytes == -1)	/* Open check */
+	{
+		free(buffer);	/* Don't forget to free memory !!! */
+		close(fd);	/* Don't forget to close file !!! */
 		return (0);
-
-	/* Output the buffer to letters bytes */
-	writeBytes = write(1, buffer, readBytes);
-	if (writeBytes == -1 || writeBytes != readBytes)
+	}
+	/* Print the buffer to read bytes */
+	write_bytes = write(1, buffer, read_bytes);
+	if (write_bytes == -1 || write_bytes != read_bytes)
+	{
+		free(buffer);	/* Don't forget to free memory !!! */
+		close(fd);	/* Don't forget to close file !!! */
 		return (0);
-
+	}
 	close(fd);	/* Close the file */
 	free(buffer);	/* Free the memory of buffer */
-
-	return (writeBytes);
+	return (write_bytes);
 }
