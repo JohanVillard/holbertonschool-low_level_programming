5148e68ad2d288e39dfa408f81eb68e6f73629f2
JohanVillard <styx.locke@gmail.com>
2024-07-30 16:26:11 +0200

    Change logic, use bitwise. Work with UINT_MAX

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-unsigned int _pow_recursion(unsigned int x, unsigned int y);
-
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: String which binary is stored
@@ -12,7 +10,7 @@ unsigned int _pow_recursion(unsigned int x, unsigned int y);
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int binary_num = 0, decimal_number = 0, binary_length = 0, i;
+	unsigned int decimal_number = 0;
 
 	if (b == NULL)
 		return (0);
@@ -21,34 +19,16 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b < '0' || *b > '1')	/* The number is base 2 */
 			return (0);
-		binary_num = 10 * binary_num + (*b - '0');	/* Convert in unsugned int */
-		b++;	/* Move 1 character */
-		binary_length++;	/* Size of the binary number */
+		/* decimal_number << 1 is equivalent to multiplying by 2 */
+		/* That means Shifting Bits Left is multiplying by 2 */
+		/* '|' add the 2 operands in binary */
+		/* *b extract the actual binary digit */
+		/* =The result is good, it's magic */
+		decimal_number = (decimal_number << 1) | (*b - '0');
+		b++;
 	}
 
-	for (i = 0; i < binary_length; i++)
-	{
-		/* Add the product if the last binary digit is not 0 */
-		decimal_number += _pow_recursion(2, i) * (binary_num % 10);
-		binary_num /= 10;	/* Cut the number */
-	}
 
 	return (decimal_number);
 }
 
-/**
-* _pow_recursion - return v power y
-* @x: unsigned int base
-* @y: unsigned int exponent
-*
-* Return: exponentiation
-*/
-unsigned int _pow_recursion(unsigned int x, unsigned int y)
-{
-	if (y == 0)
-		return (1);
-	else if (y == 0 && x == 0)
-		return (0);
-
-	return (x * _pow_recursion(x, y - 1));
-}
