8d4b5f1707086b527f2fd701353a7d629e312d1f
JohanVillard <styx.locke@gmail.com>
2024-07-31 14:47:15 +0200

    Add description to close_and_check and a pointer in it

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void close_and_check(int fd, ssize_t close_bytes);
+void close_and_check(int *fd, ssize_t *close_bytes);
 
 /**
  * main - copies the content of a file to another file.
@@ -67,18 +67,24 @@ int main(int argc, char **argv)
 	}
 	free(buffer);
 
-	close_and_check(file_to, close_bytes);	/* Close file and check if no error */
-	close_and_check(file_from, close_bytes);
+	/* Close file and check if no error */
+	close_and_check(&file_to, &close_bytes);
+	close_and_check(&file_from, &close_bytes);
 
 	return (0);
 }
 
-void close_and_check(int fd, ssize_t close_bytes)
+/**
+ * close_and_check - vlose file and check if no error
+ * @fd: is the file descriptor to close
+ * @close_bytes:  -1 if error
+ */
+void close_and_check(int *fd, ssize_t *close_bytes)
 {
-	close_bytes = close(fd);	/* Close the file descriptor */
-	if (close_bytes == -1)
+	*close_bytes = close(*fd);	/* Close the file descriptor */
+	if (*close_bytes == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_bytes);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", *close_bytes);
 		exit(100);
 	}
 }
