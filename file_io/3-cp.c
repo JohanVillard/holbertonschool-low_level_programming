a98ed4e4f87a250f9b4de227c12b7bb8a9ded8a9
JohanVillard <styx.locke@gmail.com>
2024-07-31 18:07:12 +0200

    De-refactoring -  Forgot close at th end

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -94,7 +94,23 @@ int main(int argc, char **argv)
 	free(buffer);
 
 	/* Close file and check if no error */
-	close_and_check(file_to, close_bytes);	/* exit(100) */
-	close_and_check(file_from, close_bytes);	/* exit(100) */
+	close_bytes = close(file_from);	/* Close the file descriptor */
+	if (close_bytes == -1)
+	{
+		close_bytes = close(file_to);	/* Close the file descriptor */
+		if (close_bytes == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_bytes);
+			exit(100);
+		}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_bytes);
+		exit(100);
+	}
+	close_bytes = close(file_to);	/* Close the file descriptor */
+	if (close_bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_bytes);
+		exit(100);
+	}
 	return (0);
 }
