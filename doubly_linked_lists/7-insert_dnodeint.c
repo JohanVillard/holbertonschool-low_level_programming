cca8e8b656e39c05babb0ba45d14eb1a4f199003
JohanVillard <styx.locke@gmail.com>
2024-07-31 20:25:18 +0200

    Add last index insertion

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 	}
 	if (target->next == NULL)
-		add_dnodeint_end(h, n);
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));	/* Allocate memory */
 	if (new_node == NULL)	/* Malloc check */
