ca86ed74c77cc2b5b804f8919c44a90ece91b875
JohanVillard <styx.locke@gmail.com>
2024-07-31 10:12:15 +0200

    Set up P3T2

--- file_io/main.h
+++ file_io/main.h
@@ -14,5 +14,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 /* P3T1 */
 int create_file(const char *filename, char *text_content);
+/* P3T2 */
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
