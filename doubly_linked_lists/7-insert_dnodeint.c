fd1cf4c16b5eba6abd2c110f5476bdfbd71ca182
JohanVillard <styx.locke@gmail.com>
2024-08-01 20:08:24 +0200

    Refactoring

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *curr_node;
 	unsigned int cursor = 0;	/* idx begin at 0 */
 
-
-
 	if (idx == 0)	/* Take the place 1 = idx 0 */
 	return (add_dnodeint(h, n));	/* Function creates a node at the beginning */
 
@@ -26,10 +24,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		curr_node = curr_node->next;	/* Address at idx when loop is over */
 		cursor++;
 	}
-	if (curr_node == NULL)	/* idx does not exist */
+	/* idx does not exist || idx is out of bound */
+	if (curr_node == NULL || idx - 1 > cursor)
 		return (NULL);
-	if (idx - 1 > cursor)	/* Check if idx is out of bound */
-		return (NULL);
+
 	if (curr_node->next == NULL)	/* If idx is on last node */
 		return (add_dnodeint_end(h, n)); /* Create a node at the end */
 
