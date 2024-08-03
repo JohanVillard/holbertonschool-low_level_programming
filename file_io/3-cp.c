fde1123e1e32e9972cf4dcccc4b39ee48698fa52
JohanVillard <styx.locke@gmail.com>
2024-08-03 21:56:41 +0200

    Refactoring: w_r_error_handling  function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
-void close_all(int file_from, int file_to);
+void close_all(int file1, int file2);
+void w_r_error_handling(int fp, char *buffer, char *argv,
+						int file_from, int file_to);
 
 /**
  * main - copies the content of a file to another file.
@@ -26,21 +28,12 @@ int main(int argc, char **argv)
 		return (-1);
 
 	file_from = open(argv[1], O_RDONLY);	/* Open the file at argv[1]-Read Only */
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	close_all(file_from, file_to);	/* Open error handling */
 
 	/* Create a file, trunc if exists, if not create it RW for user */
 	file_to = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, S_IRUSR
 						| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (file_to == -1)
-	{
-		close(file_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	close_all(file_to, file_from);	/* Open error handling */
 
 	buffer = malloc(1024);	/* Allocate 1024 blocks of memory to the buffer */
 	if (buffer == NULL)	/* Malloc error handling */
@@ -50,35 +43,17 @@ int main(int argc, char **argv)
 	/* Manage the case if file_from is greater than buffer */
 	while ((read_bytes = read(file_from, buffer, 1024)) > 0)
 	{
-		if (read_bytes == -1)
-		{
-			close_all(file_from, file_to);
-			free(buffer);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+		w_r_error_handling(read_bytes, buffer, argv[1],
+						file_from, file_to);
 
-		/* Write from the buffer to the file */
-		write_bytes = write(file_to, buffer, read_bytes);
-		if (write_bytes == -1)
-		{
-			close_all(file_from, file_to);
-			free(buffer);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+		write_bytes = write(file_to, buffer, read_bytes);	/* Write from the */
+		w_r_error_handling(write_bytes, buffer, argv[2],	/*buffer to the file */
+						file_from, file_to);
 	}
-
-	if (read_bytes == -1)
-	{
-		close_all(file_from, file_to);
-		free(buffer);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	w_r_error_handling(read_bytes, buffer, argv[1],
+						file_from, file_to);
 	write_bytes = 0; /* Everything is already written */
-	/* Close all files and check if no error */
-	close_all(file_from, file_to);
+	close_all(file_from, file_to);	/* Close all files and check if no error */
 	free(buffer);
 	buffer = NULL;	/* Eraser buffer */
 	return (0);
@@ -86,21 +61,42 @@ int main(int argc, char **argv)
 
 /**
  * close_all - close and check all files
- * @file_from: to close
- * @file_to: to close
+ * @file1: to close
+ * @file2: to close
  *
  * Return: Always nothing
  */
-void close_all(int file_from, int file_to)
+void close_all(int file1, int file2)
 {
-	if (close(file_from == -1))	/* If closing fails */
+	if (close(file1 == -1))	/* If closing fails */
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %u\n", file1);
 		exit(100);
 	}
-	if (close(file_to == -1))	/* If closing fails */
+	if (close(file2 == -1))	/* If closing fails */
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %u\n", file2);
 		exit(100);
+	}
+}
+
+/**
+ * w_r_error_handling - handle error
+ * if check fails after a call of write and read
+ * @fp: to check
+ * @buffer: to free
+ * @argv: where the error comes from
+ * @file_from: to close
+ * @file_to: to close
+ */
+void w_r_error_handling(int fp, char *buffer, char *argv,
+						int file_from, int file_to)
+{
+	if (fp == -1)
+	{
+		close_all(file_from, file_to);
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
+		exit(98);
 	}
 }
