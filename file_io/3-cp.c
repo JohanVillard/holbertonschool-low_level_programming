0308279a6edd021fb01ea59edd514576d7c56246
JohanVillard <styx.locke@gmail.com>
2024-08-03 22:04:24 +0200

    Fix system call check

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 void close_all(int file1, int file2);
-void w_r_error_handling(int fp, char *buffer, char *argv,
+void w_r_error_handling(char *buffer, char *argv,
 						int file_from, int file_to);
 
 /**
@@ -27,13 +27,15 @@ int main(int argc, char **argv)
 	if (argv[1] == NULL || argv[2] == NULL)
 		return (-1);
 
-	file_from = open(argv[1], O_RDONLY);	/* Open the file at argv[1]-Read Only */
-	close_all(file_from, file_to);	/* Open error handling */
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)	/* Open the file at argv[1]-Read Only */
+		close_all(file_from, file_to);	/* Open error handling */
 
 	/* Create a file, trunc if exists, if not create it RW for user */
 	file_to = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, S_IRUSR
 						| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	close_all(file_to, file_from);	/* Open error handling */
+	if (file_to == -1)
+		close_all(file_to, file_from);	/* Open error handling */
 
 	buffer = malloc(1024);	/* Allocate 1024 blocks of memory to the buffer */
 	if (buffer == NULL)	/* Malloc error handling */
@@ -43,15 +45,15 @@ int main(int argc, char **argv)
 	/* Manage the case if file_from is greater than buffer */
 	while ((read_bytes = read(file_from, buffer, 1024)) > 0)
 	{
-		w_r_error_handling(read_bytes, buffer, argv[1],
-						file_from, file_to);
+		if (read_bytes == -1)
+			w_r_error_handling(buffer, argv[1], file_from, file_to);
 
 		write_bytes = write(file_to, buffer, read_bytes);	/* Write from the */
-		w_r_error_handling(write_bytes, buffer, argv[2],	/*buffer to the file */
-						file_from, file_to);
+		if (write_bytes == -1)
+			w_r_error_handling(buffer, argv[2], file_from, file_to);
 	}
-	w_r_error_handling(read_bytes, buffer, argv[1],
-						file_from, file_to);
+	if (read_bytes == -1)
+		w_r_error_handling(buffer, argv[1], file_from, file_to);
 	write_bytes = 0; /* Everything is already written */
 	close_all(file_from, file_to);	/* Close all files and check if no error */
 	free(buffer);
@@ -83,20 +85,16 @@ void close_all(int file1, int file2)
 /**
  * w_r_error_handling - handle error
  * if check fails after a call of write and read
- * @fp: to check
  * @buffer: to free
  * @argv: where the error comes from
  * @file_from: to close
  * @file_to: to close
  */
-void w_r_error_handling(int fp, char *buffer, char *argv,
+void w_r_error_handling(char *buffer, char *argv,
 						int file_from, int file_to)
 {
-	if (fp == -1)
-	{
 		close_all(file_from, file_to);
 		free(buffer);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
 		exit(98);
-	}
 }
