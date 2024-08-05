7096d693e7dbcaf7b18466e4ed20efd88f10041c
JohanVillard <styx.locke@gmail.com>
2024-08-05 20:12:14 +0200

    Fix close aerror check

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void close_all(int file_from, int file_to);
+void error_message(ssize_t fd, int error_num, char *msg);
 
 /**
  * main - copies the content of a file to another file.
@@ -12,9 +12,8 @@ void close_all(int file_from, int file_to);
 int main(int argc, char **argv)
 {
 	int file_from = 0, file_to = 0; /* Destination file */
-	ssize_t read_bytes = 0, write_bytes = 0;
+	ssize_t read_bytes = 0, write_bytes = 0, close_byte = 0;
 	char *buffer;
-	ssize_t close_byte = 0;
 
 	/* argv[1]: 1st file ----- argv[2]: 2nd file */
 	if (argc != 3)
@@ -35,15 +34,12 @@ int main(int argc, char **argv)
 
 	/* Create a file, trunc if exists, if not create it RW for user */
 	file_to = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, S_IRUSR
-						| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 	{
-		close(file_from);
-		if (close_byte == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-			exit(100);
-		}
+		close_byte = close(file_from);
+		if (file_from == -1)
+			error_message(file_from, 100, "Error: Can't close fd");
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
@@ -60,16 +56,10 @@ int main(int argc, char **argv)
 		{
 			close_byte = close(file_from);	/* Close the file descriptor */
 			if (close_byte == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-				exit(100);
-			}
+				error_message(close_byte, 100, "Error: Can't close fd");
 			close_byte = close(file_to);	/* Close the file descriptor */
 			if (close_byte == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-				exit(100);
-			}
+				error_message(close_byte, 100, "Error: Can't close fd");
 			free(buffer);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
@@ -81,16 +71,10 @@ int main(int argc, char **argv)
 		{
 			close_byte = close(file_from);	/* Close the file descriptor */
 			if (close_byte == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-				exit(100);
-			}
+				error_message(close_byte, 100, "Error: Can't close fd");
 			close_byte = close(file_to);	/* Close the file descriptor */
 			if (close_byte == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-				exit(100);
-			}
+				error_message(close_byte, 100, "Error: Can't close fd");
 			free(buffer);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -101,16 +85,10 @@ int main(int argc, char **argv)
 	{
 		close_byte = close(file_from);	/* Close the file descriptor */
 		if (close_byte == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-			exit(100);
-		}
+			error_message(close_byte, 100, "Error: Can't close fd");
 		close_byte = close(file_to);	/* Close the file descriptor */
 		if (close_byte == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-			exit(100);
-		}
+			error_message(close_byte, 100, "Error: Can't close fd");
 		free(buffer);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -118,18 +96,24 @@ int main(int argc, char **argv)
 	write_bytes = 0; /* Everything is already written */
 	close_byte = close(file_from);	/* Close the file descriptor */
 	if (close_byte == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-		exit(100);
-	}
+		error_message(close_byte, 100, "Error: Can't close fd");
 	close_byte = close(file_to);	/* Close the file descriptor */
 	if (close_byte == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %lu\n", close_byte);
-		exit(100);
-	}
+		error_message(close_byte, 100, "Error: Can't close fd");
+
 	free(buffer);
 	buffer = NULL;	/* Eraser buffer */
 	return (0);
 }
 
+/**
+ * error_message - print eeror message
+ * @fd: file descriptor
+ * @error_num: .
+ * @msg: .
+ */
+void error_message(ssize_t fd, int error_num, char *msg)
+{
+	dprintf(STDERR_FILENO, "%s %lu\n", msg, fd);
+	exit(error_num);
+}
